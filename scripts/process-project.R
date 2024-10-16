65f8a9f960b8163d48d7f5b5af4bf1c42393f585
gen25 <gen25@cornell.edu>
2024-10-16 00:49:44 -0400

    Update the folders

---
+++ scripts/process-project.R
@@ -0,0 +1,12 @@
+
+#Upload the library
+
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(knitr)
+
+#Upaload the data
+datos <- read_excel("C:/Users/gisel/github/ntres6100_assignment_2/raw-data/wildfires_data_paraguay.xlsx")
+
+#The other analysis was made in based on Google Earth Engine
